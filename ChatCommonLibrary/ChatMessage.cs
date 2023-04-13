3b79143550d817e037b290bc2a2ead4f16f92323
Aimie <a.hardy@rubika-edu.com>
2023-04-13 16:09:45 +0200

    ChatMessage class

---
+++ ChatCommonLibrary/ChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatCommonLibrary
+{
+    public enum MessageType
+    {
+        Message,
+        Connect,
+        Disconnect,
+        Heartbeat
+    }
+
+    public class ChatMessage
+    {
+        public string message;
+        public string username;
+        public string color;
+        public MessageType type;
+    }
+}
