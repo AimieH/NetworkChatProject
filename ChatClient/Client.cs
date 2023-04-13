14869ac2a12852d2a9084754eeae95abe1a3b515
Aimie <a.hardy@rubika-edu.com>
2023-04-13 12:06:21 +0200

    Client script

--- ChatClient/Client.cs
+++ ChatClient/Client.cs
@@ -11,7 +11,27 @@ namespace ChatClient
     public class Client
     {
         private UdpClient client = new();
-        private IPEndPoint targetEndpoint = new(IPAddress.Parse("10.51.2.72"), 666);
+        private IPEndPoint? targetEndpoint = null;
+
+        private ClientForm form;
+
+        public void SetForm(ClientForm _form)
+        {
+            form = _form;
+        }
+        
+        public void ConnectToServer(string ipString)
+        {
+            if (IPAddress.TryParse(ipString, out var ipAddress))
+            {
+                targetEndpoint = new(ipAddress, 666);
+                form.DisplayError("Connected to server :)))");
+            }
+            else
+            {
+                form.DisplayError("Failed to connect to server :( - 404 :(");
+            }
+        }
 
         public void SendToServer(string message)
         {
--- ChatClient/ClientForm.cs
+++ ChatClient/ClientForm.cs
@@ -1,12 +1,14 @@
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Drawing;
 
 namespace ChatClient
 {
     public class ClientForm : Form
     {
-        private Client client = new ();
+        private Client client = new();
 
         private Button sendButton;
         private RichTextBox chatBox;
@@ -18,17 +20,27 @@ namespace ChatClient
         private TextBox usernameBox;
         private Label usernameLabel;
         private Label label1;
+        private Label ipLabel;
+        private TextBox ipTextBox;
+        private Button connectButton;
         private Color myColor = Color.Black;
 
         public ClientForm()
         {
             InitializeComponent();
 
-            sendBox.Focus();
+            client.SetForm(this);
         }
 
         private void DisplayMessage(string message)
         {
+            chatBox.AppendText(message + Environment.NewLine);
+        }
+
+        public void DisplayError(string message)
+        {
+            chatBox.SelectionColor = Color.Red;
+            chatBox.SelectionFont = new Font(chatBox.Font, FontStyle.Bold);
             chatBox.AppendText(message + Environment.NewLine);
         }
 
@@ -74,6 +86,11 @@ namespace ChatClient
             myColor = colorDialog.Color;
         }
 
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            client.ConnectToServer(ipTextBox.Text);
+        }
+
         private void InitializeComponent()
         {
             sendButton = new Button();
@@ -83,75 +100,111 @@ namespace ChatClient
             colorButton = new Button();
             usernameBox = new TextBox();
             usernameLabel = new Label();
+            ipLabel = new Label();
+            ipTextBox = new TextBox();
+            connectButton = new Button();
             SuspendLayout();
-            //
+            // 
             // sendButton
-            //
-            sendButton.Location = new Point(705, 467);
+            // 
+            sendButton.Location = new Point(832, 533);
             sendButton.Name = "sendButton";
             sendButton.Size = new Size(54, 27);
             sendButton.TabIndex = 0;
             sendButton.Text = "Send";
             sendButton.UseVisualStyleBackColor = true;
             sendButton.Click += SendButton_Click;
-            //
+            // 
             // sendBox
-            //
+            // 
             sendBox.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            sendBox.Location = new Point(12, 468);
+            sendBox.Location = new Point(12, 533);
             sendBox.Name = "sendBox";
             sendBox.PlaceholderText = "Type your message";
-            sendBox.Size = new Size(687, 26);
+            sendBox.Size = new Size(814, 26);
             sendBox.TabIndex = 2;
-            //
+            // 
             // chatBox
-            //
+            // 
             chatBox.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            chatBox.Location = new Point(12, 44);
+            chatBox.Location = new Point(12, 100);
             chatBox.Name = "chatBox";
             chatBox.ReadOnly = true;
-            chatBox.Size = new Size(747, 417);
+            chatBox.Size = new Size(874, 427);
             chatBox.TabIndex = 3;
             chatBox.Text = "";
-            //
+            // 
             // colorDialog
-            //
+            // 
             colorDialog.AnyColor = true;
-            //
+            // 
             // colorButton
-            //
+            // 
             colorButton.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            colorButton.Location = new Point(478, 12);
+            colorButton.Location = new Point(443, 56);
             colorButton.Name = "colorButton";
             colorButton.Size = new Size(158, 26);
             colorButton.TabIndex = 4;
             colorButton.Text = "Choose your  color";
             colorButton.UseVisualStyleBackColor = true;
             colorButton.Click += ColorButton_Click;
-            //
+            // 
             // usernameBox
-            //
+            // 
             usernameBox.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            usernameBox.Location = new Point(231, 12);
+            usernameBox.Location = new Point(196, 56);
             usernameBox.Name = "usernameBox";
             usernameBox.Size = new Size(241, 26);
             usernameBox.TabIndex = 5;
             usernameBox.TextChanged += UsernameBox_TextChanged;
-            //
+            // 
             // usernameLabel
-            //
+            // 
             usernameLabel.AutoSize = true;
             usernameLabel.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            usernameLabel.Location = new Point(57, 15);
+            usernameLabel.Location = new Point(22, 59);
             usernameLabel.Name = "usernameLabel";
             usernameLabel.Size = new Size(168, 18);
             usernameLabel.TabIndex = 6;
             usernameLabel.Text = "Choose your username :";
-            //
+            // 
+            // ipLabel
+            // 
+            ipLabel.AutoSize = true;
+            ipLabel.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            ipLabel.Location = new Point(113, 27);
+            ipLabel.Name = "ipLabel";
+            ipLabel.Size = new Size(77, 18);
+            ipLabel.TabIndex = 8;
+            ipLabel.Text = "Server IP :";
+            // 
+            // ipTextBox
+            // 
+            ipTextBox.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            ipTextBox.Location = new Point(196, 24);
+            ipTextBox.Name = "ipTextBox";
+            ipTextBox.Size = new Size(241, 26);
+            ipTextBox.TabIndex = 7;
+            ipTextBox.Text = "10.51.2.72";
+            // 
+            // connectButton
+            // 
+            connectButton.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            connectButton.Location = new Point(443, 23);
+            connectButton.Name = "connectButton";
+            connectButton.Size = new Size(158, 26);
+            connectButton.TabIndex = 9;
+            connectButton.Text = "Connect";
+            connectButton.UseVisualStyleBackColor = true;
+            connectButton.Click += ConnectButton_Click;
+            // 
             // ClientForm
-            //
+            // 
             BackColor = Color.Azure;
-            ClientSize = new Size(771, 502);
+            ClientSize = new Size(898, 571);
+            Controls.Add(connectButton);
+            Controls.Add(ipLabel);
+            Controls.Add(ipTextBox);
             Controls.Add(usernameLabel);
             Controls.Add(usernameBox);
             Controls.Add(colorButton);
